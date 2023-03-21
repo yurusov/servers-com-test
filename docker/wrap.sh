266d258a15ee8a78bb0f2b8d98c1358eccda0a6a
Vlad <vlad@yurusov.com>
2023-03-21 10:07:05 +0300

    refactor: used posgres compatible ENVS

--- docker/wrap.sh
+++ docker/wrap.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-PGPASSWORD=$DATABASE_PASSWORD createdb -h $DATABASE_HOST -p $DATABASE_PORT -U $DATABASE_USER -w $DATABASE_NAME || true
+createdb || true
 bundle exec rake db:migrate
 
 bundle exec $@
