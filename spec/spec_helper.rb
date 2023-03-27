c3d72e637bcd38fbcee2bf1b58d25925d044470f
Vlad <vlad@yurusov.com>
2023-03-28 00:16:59 +0300

    feat: simplecov config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,26 +1,31 @@
 # frozen_string_literal: true
 
-require 'pathname'
-SPEC_ROOT = Pathname(__dir__).realpath.freeze
-
-ENV['HANAMI_ENV'] ||= 'test'
-require 'hanami/prepare'
-require 'awesome_print'
-require 'rom-factory'
 require 'simplecov'
 require 'coverage/badge'
-
-require_relative 'support/rspec'
-require_relative 'support/requests'
-require_relative 'support/database_cleaner'
-
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
                                                                  SimpleCov::Formatter::SimpleFormatter,
                                                                  SimpleCov::Formatter::HTMLFormatter,
                                                                  Coverage::Badge::Formatter
                                                                ])
 
-SimpleCov.start
+SimpleCov.start do
+  add_group 'Actions', 'app/actions'
+  add_group 'Relations', 'lib/servers_com_test/persistence/relations'
+  add_group 'Tasks', 'lib/tasks'
+  add_filter '/spec/'
+end
+
+require 'pathname'
+SPEC_ROOT = Pathname(__dir__).realpath.freeze
+ENV['HANAMI_ENV'] ||= 'test'
+
+require 'hanami/prepare'
+require 'awesome_print'
+require 'rom-factory'
+
+require_relative 'support/rspec'
+require_relative 'support/requests'
+require_relative 'support/database_cleaner'
 
 Factory = ROM::Factory.configure do |config|
   config.rom = Hanami.app['persistence.rom']
